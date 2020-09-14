4e97c7da14baf4aef75c01367c52ab3d991c52d7
Szántó Benjámin <Beni>
2020-09-14 19:20:23 +0200

    feladat befejezve

--- szoftverfejlesztes/2020.09.08-OraiMunka/2020.09.14/gyakorlasConsole/gyakorlasConsole/Program.cs
+++ szoftverfejlesztes/2020.09.08-OraiMunka/2020.09.14/gyakorlasConsole/gyakorlasConsole/Program.cs
@@ -9,8 +9,10 @@ namespace gyakorlasConsole
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            /*
             //döntsük el egy számról hogy természetes szám-e. A nullával kezdődő szám nem természetes
             string minta = "^[1-9][0-9]*$";
             Regex pattern = new Regex(minta);
@@ -30,9 +32,33 @@ namespace gyakorlasConsole
             Console.WriteLine($"{z1}: " + ((pattern.IsMatch(z1)==true)?"helyes":"helytelen").ToString());
             Console.WriteLine($"{z2}: "+ ((pattern.IsMatch(z2) == true) ? "helyes" : "helytelen").ToString());
             Console.WriteLine($"{z3}: " + ((pattern.IsMatch(z3) == true) ? "helyes" : "helytelen").ToString());
-            Console.WriteLine();
+            Console.WriteLine();*/
 
+            double[,] temperatur = new double[12,30];
+            Random r = new Random();
+            string honap = "";
+            double min = 0, max=0;
+
+            for (int i = 0; i < temperatur.GetLength(0); i++)
+            {
+                honap = String.Empty;
+                for (int e = 0; e < temperatur.GetLength(1); e++)
+                {
+                    temperatur[i, e] = (double)r.Next(-1000, 3501) / 100;
+                    honap += temperatur[i, e] + " ";
+                    if(temperatur[i, e] < min)min = temperatur[i, e];
+                    if (temperatur[i, e] > max)max = temperatur[i, e];
+                }
+                Console.WriteLine($"{((honapok)i).ToString()}: {honap}\n" );
+            }
+
+            Console.WriteLine($"Min éves hőmérséklet: {min}");
+            Console.WriteLine($"Max éves hőmérséklet: {max}");
             Console.ReadKey();
+        }
+        enum honapok
+        {
+            jan, febr, márc, ápr, máj, jún, júl, aug, szep, okt, nov, dec
         }
     }
 }
