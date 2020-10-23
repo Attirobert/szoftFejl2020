ed0668f29830922a3b5fba349a490fb5b6cb133e
Szántó Benjámin <Beni>
2020-10-23 21:45:45 +0200

    2020.10.23

--- szoftverfejlesztes/2020.10.20 -OraiMunka/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ szoftverfejlesztes/2020.10.20 -OraiMunka/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -26,9 +26,7 @@ namespace WindowsFormsApp1
             CelList.Add(new UtazasiCel("Székesfehérvár", 2000));
             CelList.Add(new UtazasiCel("Kecskemét", 2800));
             for (int i = 0; i < CelList.Count; i++)
-            {
-                cbxCel.Items.Add(CelList[i].megnevezes);
-            }
+            { cbxCel.Items.Add(CelList[i].megnevezes); }
 
             //Kedvezmények:
             KedvezmenyList.Add(new Kedvezmenyek("-", 0));
@@ -36,18 +34,14 @@ namespace WindowsFormsApp1
             KedvezmenyList.Add(new Kedvezmenyek("nyugdíjas", 50));
             KedvezmenyList.Add(new Kedvezmenyek("törzsutas kártya", 70));
             for (int i = 0; i < KedvezmenyList.Count; i++)
-            {
-                cbxKedvezmeny.Items.Add(KedvezmenyList[i].megnevezes);
-            }
+            { cbxKedvezmeny.Items.Add(KedvezmenyList[i].megnevezes); }
 
             //Szolgáltatások:
             SzolgaltatasList.Add(new Szolgaltatasok("kutya", 800));
             SzolgaltatasList.Add(new Szolgaltatasok("bicikli", 600));
             SzolgaltatasList.Add(new Szolgaltatasok("túlméretes poggyász", 400));
             for (int i = 0; i < SzolgaltatasList.Count; i++)
-            {
-                clbSzolgaltatas.Items.Add(SzolgaltatasList[i].megnevezes);
-            }
+            { clbSzolgaltatas.Items.Add(SzolgaltatasList[i].megnevezes); }
 
             //kezdőértékek beállítása
             Reset();
@@ -58,7 +52,7 @@ namespace WindowsFormsApp1
         /// </summary>
         private void button1_Click(object sender, EventArgs e)
         {
-            int ar =CelList[cbxCel.SelectedIndex].ar;
+            int ar = CelList[cbxCel.SelectedIndex].ar;
             int kedveznmeny = KedvezmenyList[cbxKedvezmeny.SelectedIndex].ar;
             int szolgaltatas = szolgaltatasOsszeadasa();
             
@@ -80,9 +74,7 @@ namespace WindowsFormsApp1
         {
             int szam = 0;
             for (int i = 0; i < clbSzolgaltatas.CheckedItems.Count; i++)
-            {
-                szam += SzolgaltatasList[clbSzolgaltatas.Items.IndexOf(clbSzolgaltatas.CheckedItems[i])].ar;
-            }
+            { szam += SzolgaltatasList[clbSzolgaltatas.Items.IndexOf(clbSzolgaltatas.CheckedItems[i])].ar; }
             return szam;
         }
 
