e1b8262896dd81fd346cb75791482df51627b660
Szántó Benjámin <Beni>
2020-11-03 22:44:18 +0100

    feladat befejezve

---
+++ szoftverfejlesztes/2020.11.03 - OraiMunka/listaPista/listaPista/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace listaPista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.generálásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elemHozzáadásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elemTörléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(651, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generálásToolStripMenuItem,
+            this.elemHozzáadásaToolStripMenuItem,
+            this.elemTörléseToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.kilépésToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(651, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // generálásToolStripMenuItem
+            // 
+            this.generálásToolStripMenuItem.Name = "generálásToolStripMenuItem";
+            this.generálásToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.generálásToolStripMenuItem.Text = "Generálás";
+            this.generálásToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // elemHozzáadásaToolStripMenuItem
+            // 
+            this.elemHozzáadásaToolStripMenuItem.Name = "elemHozzáadásaToolStripMenuItem";
+            this.elemHozzáadásaToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.elemHozzáadásaToolStripMenuItem.Text = "Elem hozzáadása";
+            this.elemHozzáadásaToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // elemTörléseToolStripMenuItem
+            // 
+            this.elemTörléseToolStripMenuItem.Name = "elemTörléseToolStripMenuItem";
+            this.elemTörléseToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.elemTörléseToolStripMenuItem.Text = "Elem törlése";
+            this.elemTörléseToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // kilépésToolStripMenuItem
+            // 
+            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 305);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(651, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(93, 17);
+            this.toolStripStatusLabel1.Text = "Elemek száma: 0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(106, 17);
+            this.toolStripStatusLabel2.Text = "Legkissebb elem: 0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(111, 17);
+            this.toolStripStatusLabel3.Text = "legnagyobb elem: 0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 283);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 327);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem generálásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elemHozzáadásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elemTörléseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ szoftverfejlesztes/2020.11.03 - OraiMunka/listaPista/listaPista/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listaPista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            switch (((ToolStripMenuItem)sender).Text)
+            {
+                case "Generálás":
+                    Random r = new Random();
+                    for (int i = 0; i < 10; i++) listBox1.Items.Add(r.Next(10, 21));
+                    break;
+                case "Elem hozzáadása":
+                    textBox1.Visible = true;
+                    button1.Visible = true;
+                    break;
+                case "Elem törlése":
+                    if (listBox1.SelectedIndex!=-1) listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                    break;
+                case "Reset":
+                    listBox1.Items.Clear();
+                    break;
+                case "Kilépés":
+                    Close();
+                    break;
+            }
+            statusFrissitese();
+        }
+
+        private void statusFrissitese()
+        {
+            List<int> egy = new List<int>();
+            for (int i = 0; i < listBox1.Items.Count; i++) egy.Add(Convert.ToInt32(listBox1.Items[i]));
+
+            toolStripStatusLabel1.Text = (listBox1.Items.Count != 0) ? $"Elemek száma: {egy.Count}" : $"Elemek száma: {0}";
+            toolStripStatusLabel2.Text = (listBox1.Items.Count != 0) ? $"Legkissebb elem: {egy.Min()}" : $"Legkissebb elem: {0}";
+            toolStripStatusLabel3.Text = (listBox1.Items.Count != 0) ? $"Legnagyobb elem: {egy.Max()}" : $"Legnagyobb elem: {0}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            statusFrissitese();
+            textBox1.Text = String.Empty;
+            textBox1.Visible = false;
+            button1.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length!=0)
+            {
+                int number;
+                textBox1.Text=(int.TryParse(textBox1.Text, out number))? number.ToString(): textBox1.Text.TrimEnd(textBox1.Text[textBox1.Text.Length - 1]);
+            }
+        }
+
+    }
+}
