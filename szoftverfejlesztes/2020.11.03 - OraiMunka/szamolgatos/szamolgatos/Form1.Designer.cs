1ed601f293002f1cb7baebf56a67edf781ffbef0
Szántó Benjámin <Beni>
2020-11-03 19:11:48 +0100

    2020.11.03

---
+++ szoftverfejlesztes/2020.11.03 - OraiMunka/szamolgatos/szamolgatos/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace szamolgatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.számtaniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mértaniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.harmónikusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.átlagToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.legnagyobbElemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mezőkTörléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.számtaniToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mértaniaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.harmónikusToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.átlagToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.legnagyobbElemToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mezőkTörléseToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.számtaniToolStripMenuItem,
+            this.mértaniToolStripMenuItem,
+            this.harmónikusToolStripMenuItem,
+            this.átlagToolStripMenuItem,
+            this.legnagyobbElemToolStripMenuItem,
+            this.mezőkTörléseToolStripMenuItem,
+            this.kilépésToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(659, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // számtaniToolStripMenuItem
+            // 
+            this.számtaniToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("számtaniToolStripMenuItem.Image")));
+            this.számtaniToolStripMenuItem.Name = "számtaniToolStripMenuItem";
+            this.számtaniToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.számtaniToolStripMenuItem.Text = "Számtani";
+            this.számtaniToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // mértaniToolStripMenuItem
+            // 
+            this.mértaniToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("mértaniToolStripMenuItem.Image")));
+            this.mértaniToolStripMenuItem.Name = "mértaniToolStripMenuItem";
+            this.mértaniToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.mértaniToolStripMenuItem.Text = "Mértani";
+            this.mértaniToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // harmónikusToolStripMenuItem
+            // 
+            this.harmónikusToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("harmónikusToolStripMenuItem.Image")));
+            this.harmónikusToolStripMenuItem.Name = "harmónikusToolStripMenuItem";
+            this.harmónikusToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.harmónikusToolStripMenuItem.Text = "Harmónikus";
+            this.harmónikusToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // átlagToolStripMenuItem
+            // 
+            this.átlagToolStripMenuItem.Image = global::szamolgatos.Properties.Resources._001_dollar;
+            this.átlagToolStripMenuItem.Name = "átlagToolStripMenuItem";
+            this.átlagToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.átlagToolStripMenuItem.Text = "Átlag";
+            this.átlagToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // legnagyobbElemToolStripMenuItem
+            // 
+            this.legnagyobbElemToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("legnagyobbElemToolStripMenuItem.Image")));
+            this.legnagyobbElemToolStripMenuItem.Name = "legnagyobbElemToolStripMenuItem";
+            this.legnagyobbElemToolStripMenuItem.Size = new System.Drawing.Size(130, 20);
+            this.legnagyobbElemToolStripMenuItem.Text = "Legnagyobb elem";
+            this.legnagyobbElemToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // mezőkTörléseToolStripMenuItem
+            // 
+            this.mezőkTörléseToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("mezőkTörléseToolStripMenuItem.Image")));
+            this.mezőkTörléseToolStripMenuItem.Name = "mezőkTörléseToolStripMenuItem";
+            this.mezőkTörléseToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.mezőkTörléseToolStripMenuItem.Text = "Mezők törlése";
+            this.mezőkTörléseToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // kilépésToolStripMenuItem
+            // 
+            this.kilépésToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("kilépésToolStripMenuItem.Image")));
+            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 24);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(209, 24);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(382, 24);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 1;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.ValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(47, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(557, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "hibás adat";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "hibás adat";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "hibás adat";
+            this.label3.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Location = new System.Drawing.Point(47, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(567, 67);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(53, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(561, 50);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.számtaniToolStripMenuItem1,
+            this.mértaniaToolStripMenuItem,
+            this.harmónikusToolStripMenuItem1,
+            this.átlagToolStripMenuItem1,
+            this.legnagyobbElemToolStripMenuItem1,
+            this.mezőkTörléseToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(170, 136);
+            // 
+            // számtaniToolStripMenuItem1
+            // 
+            this.számtaniToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("számtaniToolStripMenuItem1.Image")));
+            this.számtaniToolStripMenuItem1.Name = "számtaniToolStripMenuItem1";
+            this.számtaniToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.számtaniToolStripMenuItem1.Text = "Számtani";
+            this.számtaniToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // mértaniaToolStripMenuItem
+            // 
+            this.mértaniaToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("mértaniaToolStripMenuItem.Image")));
+            this.mértaniaToolStripMenuItem.Name = "mértaniaToolStripMenuItem";
+            this.mértaniaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mértaniaToolStripMenuItem.Text = "Mértania";
+            this.mértaniaToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // harmónikusToolStripMenuItem1
+            // 
+            this.harmónikusToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("harmónikusToolStripMenuItem1.Image")));
+            this.harmónikusToolStripMenuItem1.Name = "harmónikusToolStripMenuItem1";
+            this.harmónikusToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.harmónikusToolStripMenuItem1.Text = "Harmónikus";
+            this.harmónikusToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // átlagToolStripMenuItem1
+            // 
+            this.átlagToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("átlagToolStripMenuItem1.Image")));
+            this.átlagToolStripMenuItem1.Name = "átlagToolStripMenuItem1";
+            this.átlagToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.átlagToolStripMenuItem1.Text = "Átlag";
+            this.átlagToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // legnagyobbElemToolStripMenuItem1
+            // 
+            this.legnagyobbElemToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("legnagyobbElemToolStripMenuItem1.Image")));
+            this.legnagyobbElemToolStripMenuItem1.Name = "legnagyobbElemToolStripMenuItem1";
+            this.legnagyobbElemToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.legnagyobbElemToolStripMenuItem1.Text = "Legnagyobb elem";
+            this.legnagyobbElemToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // mezőkTörléseToolStripMenuItem1
+            // 
+            this.mezőkTörléseToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("mezőkTörléseToolStripMenuItem1.Image")));
+            this.mezőkTörléseToolStripMenuItem1.Name = "mezőkTörléseToolStripMenuItem1";
+            this.mezőkTörléseToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.mezőkTörléseToolStripMenuItem1.Text = "Mezők törlése";
+            this.mezőkTörléseToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mértaniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem harmónikusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem átlagToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem legnagyobbElemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mezőkTörléseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem számtaniToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem számtaniToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mértaniaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem harmónikusToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem átlagToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem legnagyobbElemToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mezőkTörléseToolStripMenuItem1;
+    }
+}
+
---
+++ szoftverfejlesztes/2020.11.03 - OraiMunka/szamolgatos/szamolgatos/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szamolgatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[] szamok = {
+                    int.Parse(numericUpDown1.Value.ToString()),
+                    int.Parse(numericUpDown2.Value.ToString()),
+                    int.Parse(numericUpDown3.Value.ToString()) };
+
+            if (((ToolStripMenuItem)sender).Text!= "Mezők törlése")
+            {
+                bool kilepesMertNegativatVagyNullatTalaltam = false;
+                for (int i = 0; i < szamok.Length; i++)
+                {
+                    if (szamok[i] <= 0)
+                    {
+                        kilepesMertNegativatVagyNullatTalaltam = true;
+                        switch (i)
+                        {
+                            case 0: numericUpDown1.Focus(); label1.Visible = true; break;
+                            case 1: numericUpDown2.Focus(); label2.Visible = true; break;
+                            case 2: numericUpDown3.Focus(); label3.Visible = true; break;
+                        }
+                    }
+                }
+                if (kilepesMertNegativatVagyNullatTalaltam) return;
+            }
+            switch (((ToolStripMenuItem)sender).Text)
+            {
+                case "Számtani": listBox1.Items.Add($"{Math.Round((szamok[0] + szamok[1] + szamok[2]) /Convert.ToDouble( szamok.Length), 4)}"); break;
+                case "Mértani": listBox1.Items.Add($"Mértani: {Math.Round(Math.Pow(szamok[0] * szamok[1] * szamok[2], 1.00 / szamok.Length),4)}"); break;
+                case "Harmónikus": listBox1.Items.Add($"Harmónikus: {Math.Round( szamok.Length / ((1.00 / szamok[0]) + (1.00 / szamok[1]) + (1.00 / szamok[2])),4)}"); break;
+                case "Átlag": listBox1.Items.Add($"Átlag: {Math.Round( Convert.ToDouble(szamok[0] * szamok[1] * szamok[2]) / szamok.Length,4)}"); break;
+                case "Legnagyobb elem": listBox1.Items.Add($"Legnagyobb elem: {szamok.Max()}"); break;
+                case "Mezők törlése": for (int i = 0; i < groupBox1.Controls.Count; i++) ((NumericUpDown)groupBox1.Controls[i]).Value = 1; ; break;
+                case "Kilépés": this.Close(); break;
+            }
+        }
+
+        private void ValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < groupBox2.Controls.Count; i++) ((Label)groupBox2.Controls[i]).Visible = false;
+        }
+    }
+}
