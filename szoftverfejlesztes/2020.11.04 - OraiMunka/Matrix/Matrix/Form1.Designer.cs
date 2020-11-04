29dd780dffab7ecb5794040c76aac21f102b64f7
Szántó Benjámin <Beni>
2020-11-05 00:11:21 +0100

    2020.11.04

--- szoftverfejlesztes/2020.11.04 - OraiMunka/Matrix/Matrix/Form1.Designer.cs
+++ szoftverfejlesztes/2020.11.04 - OraiMunka/Matrix/Matrix/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.generalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.páratlanElemekÖsszegeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.muveletekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.páratlanElemekÖsszegeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.főátlókÖsszegeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sorokbanLévőElemekÖsszegeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oszlopokbanLévőElemekÖsszegeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.transzponáldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbEredmeny = new System.Windows.Forms.Label();
+            this.lbMegnevezes = new System.Windows.Forms.Label();
+            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -50,7 +52,7 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
@@ -62,12 +64,12 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.generalToolStripMenuItem,
-            this.resetToolStripMenuItem,
-            this.páratlanElemekÖsszegeToolStripMenuItem,
-            this.transzponáldToolStripMenuItem});
+            this.muveletekToolStripMenuItem,
+            this.transzponáldToolStripMenuItem,
+            this.kilépésToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1069, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(304, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -78,23 +80,16 @@
             this.generalToolStripMenuItem.Text = "Generál";
             this.generalToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
             // 
-            // resetToolStripMenuItem
+            // muveletekToolStripMenuItem
             // 
-            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
-            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
-            this.resetToolStripMenuItem.Text = "Reset";
-            this.resetToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
-            // 
-            // páratlanElemekÖsszegeToolStripMenuItem
-            // 
-            this.páratlanElemekÖsszegeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.muveletekToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.páratlanElemekÖsszegeToolStripMenuItem1,
             this.főátlókÖsszegeToolStripMenuItem,
             this.sorokbanLévőElemekÖsszegeToolStripMenuItem,
             this.oszlopokbanLévőElemekÖsszegeToolStripMenuItem});
-            this.páratlanElemekÖsszegeToolStripMenuItem.Name = "páratlanElemekÖsszegeToolStripMenuItem";
-            this.páratlanElemekÖsszegeToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
-            this.páratlanElemekÖsszegeToolStripMenuItem.Text = "Műveletek";
+            this.muveletekToolStripMenuItem.Name = "muveletekToolStripMenuItem";
+            this.muveletekToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.muveletekToolStripMenuItem.Text = "Műveletek";
             // 
             // páratlanElemekÖsszegeToolStripMenuItem1
             // 
@@ -131,11 +126,36 @@
             this.transzponáldToolStripMenuItem.Text = "Transzponáld";
             this.transzponáldToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
             // 
+            // lbEredmeny
+            // 
+            this.lbEredmeny.AutoSize = true;
+            this.lbEredmeny.Location = new System.Drawing.Point(12, 182);
+            this.lbEredmeny.Name = "lbEredmeny";
+            this.lbEredmeny.Size = new System.Drawing.Size(0, 13);
+            this.lbEredmeny.TabIndex = 2;
+            // 
+            // lbMegnevezes
+            // 
+            this.lbMegnevezes.AutoSize = true;
+            this.lbMegnevezes.Location = new System.Drawing.Point(9, 154);
+            this.lbMegnevezes.Name = "lbMegnevezes";
+            this.lbMegnevezes.Size = new System.Drawing.Size(0, 13);
+            this.lbMegnevezes.TabIndex = 3;
+            // 
+            // kilépésToolStripMenuItem
+            // 
+            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1069, 450);
+            this.ClientSize = new System.Drawing.Size(304, 261);
+            this.Controls.Add(this.lbMegnevezes);
+            this.Controls.Add(this.lbEredmeny);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -154,13 +174,15 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem generalToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem páratlanElemekÖsszegeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem muveletekToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem páratlanElemekÖsszegeToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem főátlókÖsszegeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sorokbanLévőElemekÖsszegeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oszlopokbanLévőElemekÖsszegeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem transzponáldToolStripMenuItem;
+        private System.Windows.Forms.Label lbEredmeny;
+        private System.Windows.Forms.Label lbMegnevezes;
+        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
     }
 }
 
--- szoftverfejlesztes/2020.11.04 - OraiMunka/Matrix/Matrix/Form1.cs
+++ szoftverfejlesztes/2020.11.04 - OraiMunka/Matrix/Matrix/Form1.cs
@@ -15,11 +15,34 @@ namespace Matrix
     {
         int[,] matrix = new int[3, 3];
         Random r = new Random();
+        List<string> huHU = new List<string>()
+        {
+            "Generál",
+            "Páratlan elemek összege",
+            "Főátlók összege",
+            "Sorokban lévő elemek összege",
+            "Oszlopokban lévő elemek összege",
+            "Transzponáld",
+            "Kilépés",
+            "Műveletek"
+        };
         public Form1()
         {
             InitializeComponent();
             matrixGeneralasa();
-            
+            nyelvBeallitasa(huHU);
+        }
+
+        private void nyelvBeallitasa(List<string> nyelv)
+        {
+            generalToolStripMenuItem.Text = nyelv[0];
+            páratlanElemekÖsszegeToolStripMenuItem1.Text = nyelv[1];
+            főátlókÖsszegeToolStripMenuItem.Text = nyelv[2];
+            sorokbanLévőElemekÖsszegeToolStripMenuItem.Text = nyelv[3];
+            oszlopokbanLévőElemekÖsszegeToolStripMenuItem.Text = nyelv[4];
+            transzponáldToolStripMenuItem.Text = nyelv[5];
+            kilépésToolStripMenuItem.Text = nyelv[6];
+            muveletekToolStripMenuItem.Text = nyelv[7];
         }
 
         private  void DatagridFeltoltes()
@@ -40,36 +63,100 @@ namespace Matrix
         private void matrixGeneralasa()
         {
             int[,] temp = new int[3, 3];
-            for (int i = 0; i < temp.GetLength(0); i++) 
-                for (int j = 0; j < temp.GetLength(1); j++) 
+            for (int i = 0; i < temp.GetLength(0); i++)
+            {
+                for (int j = 0; j < temp.GetLength(1); j++)
+                {
                     temp[i, j] = r.Next(10, 21);
+                } 
+            }   
             matrix = temp;
             DatagridFeltoltes();
-
+            dispatcher(lbMegnevezes.Text);
         }
 
         private void ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            switch (((ToolStripMenuItem)sender).Text)
+            dispatcher(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void dispatcher(string text)
+        {
+            if (text == huHU[0]) { matrixGeneralasa(); return; }
+            if (text == huHU[1]) { paratlan(text); return; }
+            if (text == huHU[2]) { atlokOsszege(text); return; }
+            if (text == huHU[3]) { sorokOsszege(text); return; }
+            if (text == huHU[4]) { oszlopokOsszege(text); return; }
+            if (text == huHU[5]) { transporalas(); return; }
+            if (text == huHU[6]) { Close(); }
+        }
+
+        private void oszlopokOsszege(string megnevezes)
+        {
+            lbMegnevezes.Text = megnevezes;
+            lbEredmeny.Text = String.Empty;
+            int[] Oszlopok = new int[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                case "Generál":
-                    matrixGeneralasa();
-                    break;
-                case "Reset":
-                    Reset();
-                    break;
-                case "Transzponáld":
-                    transporalas();
-                    break;
-                case "Páratlan elemek összege":
-                    break;
-                case "Főátlók összege":
-                    break;
-                case "Sorokban lévő elemek összege":
-                    break;
-                case "Oszlopokban lévő elemek összege":
-                    break;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Oszlopok[j] += matrix[i, j];
+                }
             }
+            for (int i = 0; i < Oszlopok.Length; i++)
+            {
+                lbEredmeny.Text += $"{i + 1}: {Oszlopok[i]}\n";
+            }
+        }
+
+        private void sorokOsszege(string megnevezes)
+        {
+            lbMegnevezes.Text = megnevezes;
+            lbEredmeny.Text = String.Empty;
+            int[] Sorok = new int[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Sorok[i] += matrix[i, j];
+                }
+            }
+            for (int i = 0; i < Sorok.Length; i++)
+            {
+                lbEredmeny.Text += $"{i + 1}: {Sorok[i]}\n";
+            }
+        }
+
+        private void atlokOsszege(string megnevezes)
+        {
+            lbMegnevezes.Text = megnevezes;
+            int atloEgy=0, atloKetto=0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i; j <= i; j++)
+                {
+                    atloEgy += matrix[i, j];
+                    atloKetto += matrix[i, matrix.GetLength(0) - i-1];
+                }
+            }
+            lbEredmeny.Text = $"{atloEgy + atloKetto}";
+        }
+
+        private void paratlan(string megnevezes)
+        {
+            lbMegnevezes.Text = megnevezes;
+            int eredmeny = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j =0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j]%2!=0)
+                    {
+                        eredmeny += matrix[i, j];
+                    }
+                }
+            }
+            lbEredmeny.Text = eredmeny.ToString();
         }
 
         private void transporalas()
@@ -79,27 +166,18 @@ namespace Matrix
             List<int> oszlop3 = new List<int>();
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
-
                 oszlop1.Add(matrix[i, 0]);
                 oszlop2.Add(matrix[i, 1]);
                 oszlop3.Add(matrix[i, 2]);
             }
-
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                matrix[0, j] = oszlop1[oszlop1.Count - (j)-1];
+                matrix[0, j] = oszlop1[oszlop1.Count - (j) - 1];
                 matrix[1, j] = oszlop2[oszlop2.Count - (j) - 1];
                 matrix[2, j] = oszlop3[oszlop3.Count - (j) - 1];
-                ;
             }
             DatagridFeltoltes();
-        }
-
-        private void Reset()
-        {
-            matrix = new int[3, 3];
-            DatagridFeltoltes();
-            
+            dispatcher(lbMegnevezes.Text);
         }
     }
 }
